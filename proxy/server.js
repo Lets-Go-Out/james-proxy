8ffb40ae996dddcfb3facdf016ce50d710501638
Hack Reactor Student <student@hackreactor.com>
2019-05-03 10:41:05 -0700

    new relic loader io

--- proxy/server.js
+++ proxy/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
+require("newrelic");
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.get("/loaderio-70bc2f38772bf69a689ad3dbd135c6a1.txt", (_, res) => {
+  res.sendFile(
+    path.join(__dirname, "./loaderio-70bc2f38772bf69a689ad3dbd135c6a1.txt")
+  );
+});
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
